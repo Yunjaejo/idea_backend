a82e21cd92d60eb5e6b3cfabace540c432ec5cc0
leubenia <hdodo777@gmail.com>
2021-10-11 21:24:41 +0900

    test

--- routers/comment.js
+++ routers/comment.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const posts = require("../models/post")
+const comments = require("../models/comment") 
 require('date-utils');
+
+
+router.get('/', async(req, res) => {
+    try {
+        const writes = await write.find().sort("-_Id");
+        res.json({ write: writes });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+
 
 
 module.exports = router;
