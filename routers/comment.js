89e5919d4a5c29007afbceafdfb118181151c8c7
leubenia <hdodo777@gmail.com>
2021-10-11 22:41:17 +0900

    커맨트 작성완료 postId검증해야됨

--- routers/comment.js
+++ routers/comment.js
@@ -1,17 +1,70 @@
 const express = require("express");
 const router = express.Router();
-const posts = require("../models/post")
-const comments = require("../models/comment") 
+const Comment = require("../models/comment") 
 require('date-utils');
 
+// postId 검증은 내일하자...!
 
-router.get('/', async(req, res) => {
+router.get('/:postId', async(req, res) => {
+    const { postId } = req.params; 
     try {
-        const writes = await write.find().sort("-_Id");
-        res.json({ write: writes });
+        const comment = await Comment.find({ commentTime : postId})
+        res.status(200).send({ comment: comment });
     } catch (err) {
-        console.error(err);
-        next(err);
+        res.status(400).send({ err: "코맨트 에러" });
+    }
+});
+
+router.post('/:postId', async(req, res) => {
+    const { postId } = req.params; 
+    const { nickname, comment} = req.body;
+    
+    try{
+        await comment.create({ nickname:nickname , comment: comment ,upperPost: postId});
+        res.status(200).send({ result: "success" });
+    }
+    catch (err){
+        console.log(err)
+        res.status(400).send({ err: "err" });
+    }
+});
+
+router.patch('/:commentId', async(req, res) => {
+    const { commentId } = req.params; 
+    const {email,comment} = req.body;
+    
+    const iscomment = await Comment.findById(commentId);
+    console.log(iscomment);
+    if (iscomment) {
+        if(true){
+            await Comment.updateOne({ commentId }, { $set: { email: email, comment,comment} });
+            res.status(200).send({ result: "success" });
+        }
+        else{
+            res.status(400).send({result: "err"})
+        }  
+    }
+    else{
+        res.status(400).send({result: "게시글 존재하지 않음"})
+    }
+});
+
+
+router.delete('/:commentId', async(req, res) => {
+    const { commentId } = req.params; 
+    const iscomment = await Comment.findById(commentId);
+    if (iscomment) {
+        //nickname == ispost["nickname"]
+        if(true){
+            await Comment.deleteOne({ commentId });
+            res.status(200).send({ result: "success" });
+        }
+        else{
+            res.status(400).send({result: "사용자 본인이 아님"})
+        }
+    }
+    else{
+        res.status(400).send({result: "게시글 존재하지 않음"})
     }
 });
 
