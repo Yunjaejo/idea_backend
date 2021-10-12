e42987d0de39517a475d6e153a5ff0bbd21a4fa3
yunjaejo <1a616@naver.com>
2021-10-12 12:12:35 +0900

    f

--- routers/user.js
+++ routers/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const router = express.Router(); 
-const uf = require('./userFunction.js');
+const uf = require('./userfunction.js');
 
 // 회원가입
 router.post('/signup', async (req, res) => {
